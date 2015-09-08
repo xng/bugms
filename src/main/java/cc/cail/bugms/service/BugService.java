04c9d32fa8d342f7a1a85c268f6070a4209033dd
caixiulong <coiot@foxmail.com>
2015-09-08 18:45:15 +0800

    1

---
+++ src/main/java/cc/cail/bugms/service/BugService.java
@@ -0,0 +1,7 @@
+package cc.cail.bugms.service;
+
+import cc.cail.bugms.dao.entity.Bug;
+
+public interface BugService {
+	public Bug queryById(Integer id);
+}
---
+++ src/main/java/cc/cail/bugms/service/UserService.java
@@ -0,0 +1,21 @@
+package cc.cail.bugms.service;
+
+import cc.cail.bugms.dao.entity.User;
+
+public interface UserService {
+
+	/**
+	 * 按帐号查询密码，登录用
+	 */
+	public User queryUserByAccount(String account);
+
+	/**
+	 * 保存或者更新用户
+	 */
+	public void saveOrUpdateUser(User user);
+
+	/**
+	 * 移除user
+	 */
+	public void removeUser(Integer id);
+}
